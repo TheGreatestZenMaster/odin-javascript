9878daba65c292ab9a75ac43116a55f109206641
Jake <jetloose@gmail.com>
2017-05-17 07:36:03 +0000

    Basic Image Slider Works

--- image-slider/assets/javascript/slider.js
+++ image-slider/assets/javascript/slider.js
@@ -3,28 +3,51 @@ var items = $('.slider-container img');
 var itemCount = items.length;
     
     
-var showSlide = function() {
+var showSlide = function(index) {
     $('.slider-container img').hide();
-    var current = $('.slider-container img').eq(currentIndex);
+    var current = $('.slider-container').find( 'img').eq(index);
     current.show();
 }
     
 $(document).ready(function() {
-    showSlide();
     
+    for (var i = 0; i < $('.slider-container').find('img').length; i++ ) {
+        var imageLi = $("<li></li>");
+        var slide = "slide-" + i;
+        var img = $('.slider-container').find('img').eq(i).clone();
+        imageLi.attr('id', slide).append(img)
+        $('ul').append(imageLi);
+
+        //$('#nav-buttons').find('li').eq(i);
+    }
+    showSlide(currentIndex);
     $('#next').on('click', function(event) {
-        currentIndex += 1;
-        if (currentIndex === itemCount) {
+
+        if (currentIndex === ($('.slider-container').find('img').length - 1)) {
             currentIndex = 0;
         }
-        showSlide();
+        else {
+            currentIndex += 1;
+        }
+        showSlide(currentIndex);
     })
     
     $('#previous').on('click', function(event) {
-        currentIndex -= 1;
-        if (currentIndex === -1) {
-            currentIndex = itemCount - 1;
+
+        if (currentIndex === 0) {
+            currentIndex = $('.slider-container').find('img').length - 1;
         }
-        showSlide();
+        else {
+            currentIndex -= 1;
+        }
+        showSlide(currentIndex);
+    })
+    
+    $('li').on('click', function(event){
+        var imageID = $(this).attr('id');
+        currentIndex = imageID.split('-')[1];
+        showSlide(currentIndex)
     })
 })
+
+
